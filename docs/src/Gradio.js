6716a04df142c58552ae98dc98f486a191d27baa
Jennifer <jenz33@mit.edu>
2024-03-04 09:35:28 -0500

    website

---
+++ docs/src/Gradio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Gradio = () => {
+
+    return (<div style={{ flexGrow: 1 }}>
+        <iframe
+            title="onconpc"
+            className="gradio"
+            src="https://jenniferzhou33-onconpc-visualization.hf.space"
+            frameborder="0"
+            width="850"
+            height="450"
+        ></iframe>
+    </div>);
+
+};
+
+export default Gradio;
+
+
---
+++ docs/src/Tutorial.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Tutorial = () => {
+    return (
+      <div> insert tutorial</div>
+
+  );
+};
+
+export default Tutorial;
