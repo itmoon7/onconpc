0e81ed8cff6846798446236ae64e9286431f4964
Jennifer <jenz33@mit.edu>
2024-04-08 23:39:54 -0400

    change tutorial link

--- docs/src/TutorialVideo.js
+++ docs/src/TutorialVideo.js
@@ -3,6 +3,6 @@ import YouTube from "react-youtube";
 import './TutorialVideo.css'
 
 function TutorialVideo() {
-  return <YouTube videoId="n4MbunbrnjQ" class="video-container" />;
+  return <YouTube videoId="wYl8W8SrppU" class="video-container" />;
 }
 export default TutorialVideo;
