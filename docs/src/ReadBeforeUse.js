363f3045802d3ce9c2f60b1fdcd682efeae93e06
Jennifer <jenz33@mit.edu>
2024-05-10 10:33:44 -0400

    fixed centering

--- docs/src/ReadBeforeUse.js
+++ docs/src/ReadBeforeUse.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const ReadBeforeUse = () => {
+  const listStyle = {
+    paddingLeft: "2rem", 
+  };
+    
   return (
     <div style={{ paddingTop: "2rem" }}>
       <strong>Welcome to the OncoNPC Cancer Type Prediction Tool</strong>
@@ -13,31 +17,34 @@ const ReadBeforeUse = () => {
         Please review the following important disclosures before you proceed
         with using this tool:
       </p>
-      <ul>
-        <li>
-          This OncoNPC cancer type prediction tool is based on a published
-          article in{" "}
-          <a href="https://www.nature.com/articles/s41591-023-02482-6">
-            Nature Medicine
-          </a>
-          .
-        </li>
-        <li>
-          It is currently in the experimental stage and has not been validated
-          through randomized clinical trials or prospective studies.
-        </li>
-        <li>
-          The tool is intended for research purposes only and should not be used
-          as a basis for clinical diagnosis or treatment decisions.
-        </li>
-        <li>
-          It has not been approved for clinical use by any regulatory bodies.
-        </li>
-        <li>
-          No personal data is collected by the tool, and the prediction results
-          are processed in a Hugging Face environment based on user inputs.
-        </li>
-      </ul>
+      <div style={{display: 'inline-block', textAlign: 'left'}}>
+        <ul style={listStyle}>
+          <li>
+            This OncoNPC cancer type prediction tool is based on a published
+            article in{" "}
+            <a href="https://www.nature.com/articles/s41591-023-02482-6">
+              Nature Medicine
+            </a>
+            .
+          </li>
+          <li>
+            It is currently in the experimental stage and has not been validated
+            through randomized clinical trials or prospective studies.
+          </li>
+          <li>
+            The tool is intended for research purposes only and should not be
+            used as a basis for clinical diagnosis or treatment decisions.
+          </li>
+          <li>
+            It has not been approved for clinical use by any regulatory bodies.
+          </li>
+          <li>
+            No personal data is collected by the tool, and the prediction
+            results are processed in a Hugging Face environment based on user
+            inputs.
+          </li>
+        </ul>
+      </div>
       <p>
         Please see{" "}
         <a href="https://itmoon7.github.io/onconpc/#/tutorial">this tutorial</a>{" "}
