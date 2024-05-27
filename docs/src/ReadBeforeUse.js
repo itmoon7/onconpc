e304895b583be690301251d9e21fe6e05b82e90e
Jennifer <jenz33@mit.edu>
2024-05-28 02:17:09 +0800

    recenter read before use

--- docs/src/ReadBeforeUse.js
+++ docs/src/ReadBeforeUse.js
@@ -1,29 +1,40 @@
 import React from "react";
 
 const ReadBeforeUse = () => {
-  const listStyle = {
-    paddingLeft: "2rem", 
+  const containerStyle = {
+    maxWidth: "800px", // Adjust the max width as needed
+    margin: "0 auto",
+    paddingTop: "2rem",
+    textAlign: "center",
   };
-    
+
+  const listStyle = {
+    paddingLeft: "2rem",
+    textAlign: "left",
+  };
+
   return (
-    <div style={{ paddingTop: "2rem" }}>
+    <div style={containerStyle}>
       <strong>Welcome to the OncoNPC Cancer Type Prediction Tool</strong>
       <p>
-        This tool is designed to predict the potential cancer type for a tumor among 22 cancer types, based on user-provided tumor somatic genetics information. From the user input, the tool generates features including single nucleotide variant (SNV) somatic mutations, copy number alteration (CNA) events, mutation signatures, and patient demographics (age and sex). It then utilizes a tree-based machine learning algorithm to analyze these features and produces probabilistic predictions for each likely cancer type.
+        This tool is designed to predict the potential cancer type for a tumor
+        among 22 cancer types, based on user-provided tumor somatic genetics
+        information. From the user input, the tool generates features including
+        single nucleotide variant (SNV) somatic mutations, copy number
+        alteration (CNA) events, mutation signatures, and patient demographics
+        (age and sex). It then utilizes a tree-based machine learning algorithm
+        to analyze these features and produces probabilistic predictions for
+        each likely cancer type.
       </p>
       <p>
         Please review the following important disclosures before you proceed
         with using this tool:
       </p>
-      <div style={{display: 'inline-block', textAlign: 'left'}}>
+      <div style={{ display: "inline-block", textAlign: "left" }}>
         <ul style={listStyle}>
           <li>
-            This OncoNPC cancer type prediction tool is based on our publication 
-            in{" "}
-            <a href="https://rdcu.be/dHVaq">
-              Nature Medicine
-            </a>
-            .
+            This OncoNPC cancer type prediction tool is based on our publication
+            in <a href="https://rdcu.be/dHVaq">Nature Medicine</a>.
           </li>
           <li>
             It is currently in the experimental stage and has not been validated
@@ -51,11 +62,7 @@ const ReadBeforeUse = () => {
       <p>
         For detailed information regarding the base model, performance metrics,
         implications of the predictions, utilized features, and processing,
-        please refer to{" "}
-        <a href="https://rdcu.be/dHVaq">
-          our paper
-        </a>
-        .
+        please refer to <a href="https://rdcu.be/dHVaq">our paper</a>.
       </p>
       <p>
         For feedback, questions, or to report any issues with the tool, please
